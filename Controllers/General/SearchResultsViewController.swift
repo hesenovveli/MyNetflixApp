e4f53f4b3f88075abbc8ba19b1ab061eef6d8c45
hesenovveli <velihesenov.001@gmail.com>
2024-09-09 15:18:08 +0400

    add SerachViewController

---
+++ Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResultsViewController.swift
+//  MyNetflixApp
+//
+//  Created by User on 09.09.24.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+    
+
+}
